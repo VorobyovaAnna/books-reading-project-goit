490fddc089e5c2e6b29a00bb553bc6bbb8f4230e
VorobyovaAnna <anyasonn1k@gmail.com>
2022-09-24 22:34:36 +0300

    added the theme.js

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -4,6 +4,25 @@ const theme = {
     tablet: '768px',
     desktop: '1280px',
   },
+  colors: {
+    white: '#ffffff',
+    mainBackground: '#f6f7fb',
+    inputBackground: '#f5f7fa',
+    mainFontColor: '#242a37',
+    secondaryFontColor: '#898f9f',
+    placeholderFontColor: '#a6abb9',
+    accentColor: '#ff6b08',
+    headerBackground: '#b1b5c2',
+  },
+  shadows: {
+    input: 'inset 0px 1px 2px rgba(29, 29, 27, 0.15)',
+    header: '0px 2px 3px  rgba(9, 30, 63, 0.1)',
+    google: '0px 2px 2px rgba(9, 30, 63, 0.15)',
+    hero: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+    modal: '10px 10px 20px rgba(9, 30, 63, 0.2)',
+    instrustion: '0px 2px 4px rgba(0, 0, 0, 0.25)',
+    goals: '4px 4px 8px rgba(36, 42, 55, 0.15)',
+  },
 };
 
 export default theme;
