f0632284edea39a224f99ac9a24e133f22ba1296
VladPomynchuk <pomynchuk123@gmail.com>
2022-09-27 15:25:14 +0300

    feat/add breackpoint

--- src/components/Library/Library.jsx
+++ src/components/Library/Library.jsx
@@ -6,21 +6,36 @@ import ReadTable from './ReadTable/ReadTable';
 import { tableData } from './Table/data';
 import { tableData2 } from './Table/data';
 import { tableData3 } from './ReadTable/data';
+import { useMatchMedia } from 'hooks';
 
 const Library = () => {
+  const { isMobile } = useMatchMedia();
+
+  console.log(isMobile);
   return (
     <>
       <LibraryForm />
-      <ReadTable title={'Прочитано'} status={false} data={tableData3} />
-      <Table title={'Читаю'} status={true} data={tableData} />
-      <Table title={'Маю намір прочитати'} status={false} data={tableData2} />
-      <MobileTable title={'Прочитано'} status={false} data={tableData3} />
-      <MobileTable title={'Читаю'} status={true} data={tableData} />
-      <MobileTable
-        title={'Маю намір прочитати'}
-        status={false}
-        data={tableData2}
-      />
+      {isMobile ? (
+        <>
+          <MobileTable title={'Прочитано'} status={false} data={tableData3} />
+          <MobileTable title={'Читаю'} status={true} data={tableData} />
+          <MobileTable
+            title={'Маю намір прочитати'}
+            status={false}
+            data={tableData2}
+          />
+        </>
+      ) : (
+        <>
+          <ReadTable title={'Прочитано'} status={false} data={tableData3} />
+          <Table title={'Читаю'} status={true} data={tableData} />
+          <Table
+            title={'Маю намір прочитати'}
+            status={false}
+            data={tableData2}
+          />
+        </>
+      )}
     </>
   );
 };
